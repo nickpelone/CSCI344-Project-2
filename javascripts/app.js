ec22cd6dc9972c03ef9538c2b8dfd2c01b17ba22
Nick Pelone <npelone@unca.edu>
2013-02-28 18:53:28 -0500

    Adds input form and Go! button which then calls ctwitter with the user input as it's search term

--- javascripts/app.js
+++ javascripts/app.js
@@ -1,14 +1,20 @@
 (function () {
   "use strict";
-  var $ = window.$, ctwitter = window.ctwitter, userInput = "English";
+  var $ = window.$, ctwitter = window.ctwitter, userInput;
   $(document).ready((function () {
     alert("Jquery loaded");
-    //now lets set up a CTwitter
-    var twitter = new ctwitter.CTwitter();
-    twitter.stream("statuses/filter", { lang: "en", track: [userInput] }, function (stream) {
-      stream.on("data", function (tweet) {
-        //we do stuff with the tweets here
-        console.log(tweet.text);
+    //let's grab some user input and wait for a signal before we start getting from twitter
+    //we will start by adding a click handler to the go button, which when clicked,
+    //we will take the value and search through twitter with it
+    $("#go").click(function () {
+      userInput = $("#searchBox").val();
+      //now lets set up a CTwitter
+      var twitter = new ctwitter.CTwitter();
+      twitter.stream("statuses/filter", { lang: "en", track: [userInput] }, function (stream) {
+        stream.on("data", function (tweet) {
+          //we do stuff with the tweets here
+          console.log(tweet.text);
+        });
       });
     });
   }()));
