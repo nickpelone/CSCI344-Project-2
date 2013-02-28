83ed180379ec4f629d5619b5b95762597350ec69
Nick Pelone <npelone@unca.edu>
2013-02-28 18:27:40 -0500

    Moved ctwitter to appropriate location, adds Ctwitter into page with valid, lint-passing javascript, and logs output from ctwitter to console for debug purposes

--- javascripts/app.js
+++ javascripts/app.js
@@ -1,7 +1,15 @@
 (function () {
   "use strict";
-  var $ = window.$;
+  var $ = window.$, ctwitter = window.ctwitter, userInput = "English";
   $(document).ready((function () {
     alert("Jquery loaded");
+    //now lets set up a CTwitter
+    var twitter = new ctwitter.CTwitter();
+    twitter.stream("statuses/filter", { lang: "en", track: [userInput] }, function (stream) {
+      stream.on("data", function (tweet) {
+        //we do stuff with the tweets here
+        console.log(tweet.text);
+      });
+    });
   }()));
 }());
